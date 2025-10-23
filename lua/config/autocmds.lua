dcf8cb493d74287571124f4a3aacbd12803be0bf
Michael Le <hle.michael@gmail.com>
2025-10-23 23:57:53 +0100

    fix autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,6 +8,10 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 --
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
 -- Automatically organise imports on save for TypeScript files
 vim.api.nvim_create_autocmd("BufWritePre", {
   group = augroup("organise_imports"),
